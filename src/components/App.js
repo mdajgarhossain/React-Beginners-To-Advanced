1e986b8352de1718c183304dfd8ea0eac25cf68d
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-11-06 18:37:10 +0600

    Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,19 @@ import Parent from './MyComponents/ParentComponent';
 import NestedComponents from './MyComponents/NestedComponents';
 import CompoWithProps from './MyComponents/CompoWithProps';
 import Person from './MyComponents/DestructuringProps';
+import Navbar from './MyComponents/Navbar';
 
 function App() {
   return (
     <div className="App">
-      {/* <Parent />
-      <NestedComponents /> */}
+      <Navbar />
+      
+      <Parent />
+      <NestedComponents />
 
-      {/* <CompoWithProps name="Jewell" profession="Software Developer"/>
+      <CompoWithProps name="Jewell" profession="Software Developer"/>
       <CompoWithProps name="Karim" profession="Businessman"/>
-      <CompoWithProps name="Rahim" profession="Traveler"/> */}
+      <CompoWithProps name="Rahim" profession="Traveler"/>
 
       <Person name='Ajgar' age={25} profession='Software Developer'/>
     </div>
---
+++ src/components/MyComponents/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Make my own navbar
+
+const Navbar = (props) => {
+    return(
+        <div>
+            <nav>
+                <button>Home</button>
+                <button>Blog</button>
+                <button>About</button>
+                <button>Contact</button>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
